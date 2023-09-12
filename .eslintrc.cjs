f97cddaa5965e3b33361da96a1cc33beb87991c9
Paulo Dias <paulo.dias@trin.ca>
2023-09-12 17:44:40 -0300

    add rules eslint to build

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,36 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "@typescript-eslint/no-explicit-any": "off",
+
+    // Ignorar erros relacionados a variáveis não utilizadas
+    "no-unused-vars": "off",
+
+    // Ignorar erros relacionados a variáveis não utilizadas no ESLint
+    "@typescript-eslint/no-unused-vars": "off",
+
+    // Ignorar erros relacionados a propriedades desconhecidas em objetos literais
+    "@typescript-eslint/no-unused-vars": "off",
+
+    // Ignorar erros relacionados a tipos desconhecidos ou implícitos
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+
+    // Ignorar erros relacionados a "any" implícito
+    "@typescript-eslint/no-implicit-any-catch": "off",
+
+    // Ignorar erros relacionados a "any" implícito em funções
+    "@typescript-eslint/no-implicit-any": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
